438f3b679b8406201c2b5faf04b9c5f83c04068e
fusionforscience <fn.matthew@outlook.com>
2019-06-07 22:12:00 -0700

    Working POSIX compliant

--- install-commands.sh
+++ install-commands.sh
@@ -30,13 +30,22 @@ do
     fi
 done
 
+echo
 echo "Installing..."
 
 commands=$(find ./commands -type f -executable)
 
+echo
 echo "$commands"
 
-for command in "$commands"
+i=0
+lines=$(echo "$commands" | wc -l)
+while [ $i -lt $lines ]
 do
-    cp "$command" ~/Desktop
+    currentTailAmount=$(echo "$lines - $i" | bc)
+    currentCommand="$(echo "$commands" | tail -n $currentTailAmount | head -n 1)"
+    
+    cp "$currentCommand" "$installPath"
+
+    i=$(echo $i + 1 | bc)
 done
